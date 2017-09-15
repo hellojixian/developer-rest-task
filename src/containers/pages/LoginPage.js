26cdf61f17620025e44bdcf9bd9d412b77ce1a51
Jixian Wang <hellojixian@gmail.com>
2017-09-15 14:38:36 +0200

    inital commit

---
+++ src/containers/pages/LoginPage.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+import LoginPageComponenet from '../../components/pages/LoginPage';
+
+
+// Which props do we want to inject, given the global state?
+function select(state) {
+  return {
+    data: state.auth
+  };
+}
+
+// Wrap the component to inject dispatch and state into it
+export default connect(select)(LoginPageComponenet);
