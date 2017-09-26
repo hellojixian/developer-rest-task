7d412c268fd621789060338c7aa3ce9c8b31f69f
james tak <jtak93@gmail.com>
2017-09-26 15:44:54 -0700

    adds modal popup

--- src/components/client-dashboard/Product.js
+++ src/components/client-dashboard/Product.js
@@ -1,22 +1,81 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Grid, Row, Col } from 'react-bootstrap';
+import Modal from '../common/Modal'
+
+const dialogStyle = {
+    position: 'absolute',
+    width: '50vw',
+    minWidth: '600px',
+    top: '10vh',
+    left: '25vw',
+    borderRadius: '5px',
+    backgroundColor: 'white',
+    boxShadow: '0 5px 15px rgba(0,0,0,.5)',
+    padding: 20
+}
 
 class Product extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            modalOpen: false
+        }
+        this.handleProductClick = this.handleProductClick.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+    }
     handleProductClick() {
-        console.log('clicked')
+        this.setState({modalOpen: true});
+    }
+
+    closeModal() {
+        this.setState({modalOpen: false});
     }
 
     render(){
         return (
             <div className='product-container' onClick={this.handleProductClick}>
+                <Modal
+                    showModal={this.state.modalOpen}
+                    onHide={this.closeModal}
+                >
+                    <div style={dialogStyle} >
+                        <Grid fluid>
+                            <Row>
+                                <Col xs={2}>
+                                    <img height="60" width="60" className='product-barcode' src={'https://d30y9cdsu7xlg0.cloudfront.net/png/52715-200.png'} />
+                                </Col>
+                                <Col xs={9}>
+                                    <h4 className='product-name brand-blue'>{this.props.product.name}</h4>
+                                    <h5 className='product-short-desc'>{this.props.product.shortDesc}</h5>
+                                    <h6 className='product-date'>Since {this.props.product.date}</h6>
+                                </Col>
+                                <Col xs={1}>
+                                    <button>x</button>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs={4}>
+                                    <img style={{marginTop: 20}} height="120" width="120" className='product-barcode' src={"http://via.placeholder.com/120x120"} />
+                                </Col>
+                                <Col xs={8}>
+                                    <h4 className='brand-blue'>Summary</h4>
+                                    <p>
+                                        {this.props.product.summary}
+                                    </p>
+                                </Col>
+                            </Row>
+                        </Grid>
+                    </div>
+                </Modal>
                 <Grid fluid>
                     <Row>
                         <Col xs={3} >
                             <img height="60" width="60" className='product-barcode' src={'https://d30y9cdsu7xlg0.cloudfront.net/png/52715-200.png'} />
                         </Col>
                         <Col xs={6} >
-                            <h5 className='product-name'>{this.props.product.name}</h5>
+                            <h5 className='product-name brand-blue'>{this.props.product.name}</h5>
                             <h6 className='product-short-desc'>{this.props.product.shortDesc}</h6>
                             <h6 className='product-date'>Since {this.props.product.date}</h6>
                         </Col>
